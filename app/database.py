527df9fe579d55bb9e36ebfcf110e41f8fd245ec
Denis Jongmanee <jongmanee.denis@gmail.com>
2024-06-14 16:46:08 +0200

    refactor(connexion): Utilisation de la variable d'environement ENVIRONMENT_MODE pour choisir la connexion entre la classique et l'unix socket

--- app/database.py
+++ app/database.py
@@ -10,27 +10,36 @@ load_dotenv()
 database_username = os.getenv('DATABASE_USERNAME')
 database_password = os.getenv('DATABASE_PASSWORD')
 unix_socket_path  = os.getenv('INSTANCE_UNIX_SOCKET')
+environment_mode = os.getenv('ENVIRONMENT_MODE')
+database_host = os.getenv("DATABASE_HOST")
 
-## connexion pour le local
-# database_host = os.getenv("DATABASE_HOST")
-# database_url = f"mysql+pymysql://{database_username}:" \
-#     f"{database_password}@{database_host}/paye-ton-kawa"
-# engine = sqlalchemy.create_engine(database_url, echo=True)
+def engine_dev():
+    """
+        Connexion à la base de donnée dev
+    """
+    database_url = f"mysql+pymysql://{database_username}:" \
+        f"{database_password}@{database_host}/paye-ton-kawa"
+    return sqlalchemy.create_engine(database_url, echo=True)
 
-## connexion pour le cloud run
-engine = sqlalchemy.create_engine(
-    sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=database_username,
-        password=database_password,
-        database="paye-ton-kawa",
-        host="",
-        port=None,
-        query={
-            "unix_socket": unix_socket_path
-        }
+def engine_cloud():
+    """
+        Connexion à la base de données cloud sql
+    """
+    return sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL(
+                drivername="mysql+pymysql",
+                username=database_username,
+                password=database_password,
+                database="paye-ton-kawa",
+                host="",
+                port=None,
+                query={
+                "unix_socket": unix_socket_path
+            }
+        )
     )
-)
+
+engine = engine_dev() if environment_mode == "dev" else engine_cloud()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
