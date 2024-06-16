db2267172559a070dce22884a1a77736cd27835f
DenisJongmanee <jongmanee.denis@gmail.com>
2024-06-16 12:44:31 +0200

    feat(client): Ajout de la route pour récupérer un client par son id

--- app/routers/client.py
+++ app/routers/client.py
@@ -7,12 +7,22 @@ from app import actions, schemas
 router = APIRouter()
 
 @router.get("", response_model=List[schemas.Client])
-async def get_client(database: Session = Depends(get_db)):
+async def get_clients(database: Session = Depends(get_db)):
     """
         Retourne tous les clients
     """
-    try:
-        db_clients = actions.get_clients(database)
-        return db_clients
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+    db_clients = actions.get_clients(database)
+
+    return db_clients
+
+@router.get("/{id_client}", response_model=schemas.Client)
+async def get_client(id_client: int, database: Session = Depends(get_db)):
+    """
+        Retourne le client trouvé par son id
+    """
+    db_client = actions.get_client(id_client, database)
+
+    if db_client is None:
+        raise HTTPException(status_code=404, detail="Client not found")
+
+    return db_client
