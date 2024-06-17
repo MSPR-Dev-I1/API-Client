770b240031df826aae80f49b6b416c96275dc128
DenisJongmanee <jongmanee.denis@gmail.com>
2024-06-17 21:54:31 +0200

    feat(client): Ajout de la route de création d'un nouveau client

--- app/routers/client.py
+++ app/routers/client.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app import actions, schemas
+from app import actions, schemas, models
 
 router = APIRouter()
 
@@ -32,5 +32,25 @@ async def get_client(id_client: int, database: Session = Depends(get_db)):
         return db_client
     except HTTPException as http_exc:
         raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
+
+@router.post("", response_model=schemas.Client, status_code=201)
+async def post_client(client: schemas.ClientCreate, database: Session = Depends(get_db)):
+    """
+        Créer un nouveau client
+    """
+    try:
+        new_client = models.Client(
+            nom=client.nom,
+            prenom=client.prenom,
+            email=client.email,
+            adresse=client.adresse,
+            code_postal=client.code_postal,
+            ville=client.ville,
+        )
+        db_client = actions.create_client(new_client, database)
+
+        return db_client
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
--- app/test/test_client.py
+++ app/test/test_client.py
@@ -1,7 +1,23 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from app.main import app
+from app import models, actions
+from app.models import Base
 
 client = TestClient(app)
+
+def memory_engine():
+    """
+        Créer un engine sqlalchemy qui utilise la mémoire
+    """
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    session_factory = sessionmaker(bind=engine)
+    database = scoped_session(session_factory)
+
+    return database
 
 def test_get_clients(mocker):
     """
@@ -76,5 +92,84 @@ def test_get_client_error_500(mocker):
     mocker.patch("sqlalchemy.orm.Session.query", side_effect=Exception("Connection error"))
 
     response = client.get("/client/1")
+
+    assert response.status_code == 500
+
+def test_post_client(mocker):
+    """
+        Cas passant (retourne le client avec un id)
+    """
+    new_client = {
+        "nom": "test",
+        "prenom": "test",
+        "email": "test.test@ecoles-epsi.net",
+        "adresse": "9 rue de la Monnaie",
+        "code_postal": "59000",
+        "ville": "Lille"
+    }
+    db_client = models.Client(
+        id_client=1,
+        nom=new_client['nom'],
+        prenom=new_client['prenom'],
+        email=new_client['email'],
+        adresse=new_client['adresse'],
+        code_postal=new_client['code_postal'],
+        ville=new_client['ville']
+    )
+
+    mocker.patch("app.actions.create_client", return_value=db_client)
+
+    response = client.post("/client", json=new_client)
+
+    assert response.status_code == 201
+
+def test_action_create_client():
+    """
+        Test unitaire de la function create client
+    """
+    database = memory_engine()
+    new_client = models.Client(
+        nom="test",
+        prenom="test",
+        email="test.test@ecoles-epsi.net",
+        adresse="9 rue de la Monnaie",
+        code_postal="59000",
+        ville="Lille"
+    )
+
+    db_client = actions.create_client(new_client, database)
+    assert isinstance(db_client, models.Client)
+    assert db_client.id_client is not None
+
+def test_post_client_error_422():
+    """
+        Cas non passant (des informations du client sont manquants)
+    """
+    new_client = {
+        "nom": "test",
+        "adresse": "9 rue de la Monnaie",
+        "code_postal": "59000",
+        "ville": "Lille"
+    }
+
+    response = client.post("/client", json=new_client)
+
+    assert response.status_code == 422
+
+def test_post_client_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    new_client = {
+        "nom": "test",
+        "prenom": "test",
+        "email": "test.test@ecoles-epsi.net",
+        "adresse": "9 rue de la Monnaie",
+        "code_postal": "59000",
+        "ville": "Lille"
+    }
+    mocker.patch("sqlalchemy.orm.Session.commit", side_effect=Exception("Connection error"))
+
+    response = client.post("/client", json=new_client)
 
     assert response.status_code == 500
