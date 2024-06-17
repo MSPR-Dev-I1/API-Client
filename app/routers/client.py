fc52e057fea7fb3e222aa6e14ffaf8045d1a85d8
DenisJongmanee <jongmanee.denis@gmail.com>
2024-06-17 19:28:02 +0200

    fix(test) : Correction des tests unitaires pour prendre en compte actions.py dans le coverage + Ajout des tests erreur 500

--- app/routers/client.py
+++ app/routers/client.py
@@ -11,18 +11,26 @@ async def get_clients(database: Session = Depends(get_db)):
     """
         Retourne tous les clients
     """
-    db_clients = actions.get_clients(database)
+    try:
+        db_clients = actions.get_clients(database)
 
-    return db_clients
+        return db_clients
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
 
 @router.get("/{id_client}", response_model=schemas.Client)
 async def get_client(id_client: int, database: Session = Depends(get_db)):
     """
         Retourne le client trouvé par son id
     """
-    db_client = actions.get_client(id_client, database)
+    try:
+        db_client = actions.get_client(id_client, database)
 
-    if db_client is None:
-        raise HTTPException(status_code=404, detail="Client not found")
+        if db_client is None:
+            raise HTTPException(status_code=404, detail="Client not found")
 
-    return db_client
+        return db_client
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Connection failed: {e}") from e
--- app/test/test_client.py
+++ app/test/test_client.py
@@ -16,16 +16,26 @@ def test_get_clients(mocker):
         "code_postal": "59000",
         "ville": "Lille"
     }]
-    mocker.patch("app.actions.get_clients", return_value=clients)
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=clients)
 
     response = client.get("/client")
 
     assert response.status_code == 200
     assert response.json() == clients
 
+def test_get_clients_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    mocker.patch("sqlalchemy.orm.Session.query", side_effect=Exception("Connection error"))
+
+    response = client.get("/client")
+
+    assert response.status_code == 500
+
 def test_get_client(mocker):
     """
-        Cas passant (retorune un client)
+        Cas passant (retourne un client)
     """
     db_client = {
        "id_client": 1,
@@ -36,7 +46,9 @@ def test_get_client(mocker):
         "code_postal": "59000",
         "ville": "Lille"
     }
-    mocker.patch("app.actions.get_client", return_value=db_client)
+    mock_query = mocker.MagicMock()
+    mock_query.where.return_value.first.return_value = db_client
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=mock_query)
 
     response = client.get("/client/" + str(db_client['id_client']))
 
@@ -48,8 +60,21 @@ def test_get_client_error_404(mocker):
         Cas non passant (le client n'a pas été trouvé)
     """
     db_client = None
-    mocker.patch("app.actions.get_client", return_value=db_client)
+    mock_query = mocker.MagicMock()
+    mock_query.where.return_value.first.return_value = db_client
+    mocker.patch("sqlalchemy.orm.Session.query", return_value=mock_query)
 
     response = client.get("/client/1")
 
     assert response.status_code == 404
+    assert response.json() == {'detail': 'Client not found'}
+
+def test_get_client_error_500(mocker):
+    """
+        Cas non passant (erreur sur la connexion sur la base de données)
+    """
+    mocker.patch("sqlalchemy.orm.Session.query", side_effect=Exception("Connection error"))
+
+    response = client.get("/client/1")
+
+    assert response.status_code == 500
